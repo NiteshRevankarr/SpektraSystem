b3d3c807071ffd115b70364ec4eeadc29a2448c3
NiteshRevankarr <niteshrevankar6@gmail.com>
2023-04-13 14:09:07 +0530

    Test- 13-04-2023

---
+++ Angular Assignment/Test/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+
+const routes: Routes = [
+  {path:'',component:HeaderComponent},
+  {path:'home',component:HomeComponent},
+  {path:'menu',component:MenuComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
